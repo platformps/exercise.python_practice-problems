344381ef6673e0b0e46423c05093bdbe013c4ac9
git-leon <xleonhunter@gmail.com>
2020-12-08 14:21:51 -0500

    finished solution

--- src/main/lister.py
+++ src/main/lister.py
@@ -1,36 +1,25 @@
 # Created by Leon Hunter at 12:10 PM 12/08/2020
 class Lister(object):
     def get_integer_list(self, start, stop, step):
-        ''' TODO - Implement solution
-        Given 3 integers,
-            `start`, `stop`, and `step`
-        return a list of all integers
-            between `start`,
-            incrementing by `step`,
-            up to and including `stop`
-        '''
-        return None
+        if(step != 0):
+            result = []
+            for value in range(start, stop+1, step):
+                result.append(value)
+            return result
+        return [0]
 
     def get_even_list(self, start, stop, step):
-        ''' TODO - Implement solution
-        Given 3 integers,
-            `start`, `stop`, and `step`
-        return a list of all integers
-            between `start`,
-            incrementing by `step`,
-            up to and including `stop`
-            and are divisible by 2
-        '''
-        return None
+        result = []
+        for value in self.get_integer_list(start, stop, step):
+            is_even = value % 2 == 0
+            if(is_even):
+                result.append(value)
+        return result
 
     def get_odd_list(self, start, stop, step):
-        ''' TODO - Implement solution
-        Given 3 integers,
-            `start`, `stop`, and `step`
-        return a list of all integers
-            between `start`,
-            incrementing by `step`,
-            up to and including `stop`
-            and are not divisible by 2
-        '''
-        return None
+        result = []
+        for value in self.get_integer_list(start, stop, step):
+            is_odd = value % 2 == 1
+            if(is_odd):
+                result.append(value)
+        return result
