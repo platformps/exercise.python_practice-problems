69efa8259e5bf9e1ea8ab5e9152b300f73032279
DanyYanez <dany2332@hotmail.es>
2020-12-08 14:37:52 -0500

    Solved

--- src/main/lister.py
+++ src/main/lister.py
@@ -1,36 +1,31 @@
 # Created by Leon Hunter at 12:10 PM 12/08/2020
 class Lister(object):
     def get_integer_list(self, start, stop, step):
-        ''' TODO - Implement solution
-        Given 3 integers,
-            `start`, `stop`, and `step`
-        return a list of all integers
-            between `start`,
-            incrementing by `step`,
-            up to and including `stop`
-        '''
-        return None
+        number = []
+        if start == stop and stop == step:
+            number.append(start)
+        else:
+            while start <= stop:
+                number.append(start)
+                start += step
+        return number
 
     def get_even_list(self, start, stop, step):
-        ''' TODO - Implement solution
-        Given 3 integers,
-            `start`, `stop`, and `step`
-        return a list of all integers
-            between `start`,
-            incrementing by `step`,
-            up to and including `stop`
-            and are divisible by 2
-        '''
-        return None
+        number = []
+        if start == stop and stop == step:
+            number.append(start)
+        else:
+            while start <= stop:
+                if start % 2 == 0:
+                    number.append(start)
+                start += step
+        return number
 
     def get_odd_list(self, start, stop, step):
-        ''' TODO - Implement solution
-        Given 3 integers,
-            `start`, `stop`, and `step`
-        return a list of all integers
-            between `start`,
-            incrementing by `step`,
-            up to and including `stop`
-            and are not divisible by 2
-        '''
-        return None
+        number = []
+        if start != stop and stop != step:
+            while start <= stop:
+                if start % 2 != 0:
+                    number.append(start)
+                start += step
+        return number
