3dbf154ee585f98f5bb28100a6995d59f61e92d1
git-leon <xleonhunter@gmail.com>
2020-12-08 14:23:00 -0500

    completed solution

--- src/main/lister.py
+++ src/main/lister.py
@@ -1,7 +1,7 @@
 # Created by Leon Hunter at 12:10 PM 12/08/2020
 class Lister(object):
     def get_integer_list(self, start, stop, step):
-        if(step != 0):
+        if step != 0:
             result = []
             for value in range(start, stop+1, step):
                 result.append(value)
@@ -12,7 +12,7 @@ class Lister(object):
         result = []
         for value in self.get_integer_list(start, stop, step):
             is_even = value % 2 == 0
-            if(is_even):
+            if is_even:
                 result.append(value)
         return result
 
@@ -20,6 +20,6 @@ class Lister(object):
         result = []
         for value in self.get_integer_list(start, stop, step):
             is_odd = value % 2 == 1
-            if(is_odd):
+            if is_odd:
                 result.append(value)
         return result
